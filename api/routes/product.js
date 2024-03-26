a43599ba0ee349467572b61855469442ca6c1dae
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-03-26 15:42:35 +0100

    fixed

--- api/routes/product.js
+++ api/routes/product.js
@@ -2,7 +2,7 @@
 const router = require('express').Router();
 const {verfiyTokenAndAutho, verfiyTokenAndAdmin} = require('../middleware/verifyToken'); 
 const Product = require('../models/Product');
-const Product = require('../models/Product'); 
+
 
 
 
