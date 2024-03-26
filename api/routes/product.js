f1ff1833b0e8a9e23acf2e222d24c9310cf48ff3
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-03-26 15:11:41 +0100

    delete and retrive all products route

--- api/routes/product.js
+++ api/routes/product.js
@@ -47,6 +47,36 @@ router.put('/:id' , verfiyTokenAndAdmin , async (req , res)=>{
 
 
 
+router.delete('/:id' , verfiyTokenAndAdmin , async (req , res)=>{
+    try {
+        const {id} = req.params; 
+
+        const deletedProd = await Product.findByIdAndRemove(id); 
+
+
+        res.status(200).json({message : "deleted"}); 
+    } catch (error) {
+        res.status(500).json({message : error}); 
+    }
+});
+
+
+
+
+router.get('/all' , async (req,res)=>{
+    try {
+        const products = await Product.find(); 
+        res.status(200).json(products); 
+    } catch (error) {
+        res.status(500).json(error); 
+    }
+});
+
+
+
+
+
+
 
 
 
