f56f6aef799e0e2464c13da999049b611e3fb8cc
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-03-26 07:31:03 +0100

    login func using JWT

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,7 @@
 //Router
 const router = require('express').Router();
 const bcrypt = require('bcrypt'); 
+const jwt = require('jsonwebtoken'); 
 
 
 
@@ -35,6 +36,49 @@ router.post('/register' , async (req ,res)=>{
 
         //sending result to client 
         res.status(201).json(savedUser); 
+
+
+    } catch (error) {
+        res.status(500).json(error); 
+    }
+}); 
+
+
+
+
+router.post('/login' , async (req , res)=>{
+    const {username , password} = req.body ; 
+
+
+    try {
+        const user = await User.findOne({username : username}); 
+
+
+        // if the user don't exist 
+        if(!user){
+            res.status(401).json({message : "unauth"}); 
+        }
+
+
+        //checking the password 
+        const isPassValid = await bcrypt.compare(password , user.password);
+
+
+        // if the passwor is invalid 
+        if(!isPassValid){
+            res.status(401).json({message : "unauth"});
+        }
+
+
+        //creating a token for the valid user 
+        const accessToken = jwt.sign({
+            id : user.id ,  isAdmin : user.isAdmin, 
+        }, process.env.JWT_SECRET , {expiresIn :"3d"}); 
+
+
+
+        const {pass , isAdmin , ...others} = user._doc ; 
+        res.status(200).json({...others , accessToken}); 
 
 
     } catch (error) {
