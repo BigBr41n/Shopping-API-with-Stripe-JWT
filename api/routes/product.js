002cdfd182e6dccde4da4d4fc655497fd95b4cd2
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-03-26 14:59:48 +0100

    create new prod route

--- api/routes/product.js
+++ api/routes/product.js
@@ -1,13 +1,22 @@
 //Router
 const router = require('express').Router();
+const {verfiyTokenAndAutho, verfiyTokenAndAdmin} = require('../middleware/verifyToken'); 
+const User = require('../models/Product'); 
 
 
 
 
+router.post('/' , verfiyTokenAndAdmin , async (req , res)=>{
+    try {
+        const newProd = new Product(req.body); 
+        const savedProd = await newProd.save(); 
 
-router.get("/" , (req , res)=>{
-    res.send('true'); 
-}); 
+        res.status(200).json(savedProd); 
+
+    } catch (error) {
+        res.status(500).json({message : error}); 
+    }
+});
 
 
 
