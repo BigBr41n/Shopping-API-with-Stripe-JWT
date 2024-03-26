906a24874ef5f4ea9aad6213998ef555ef2e7abd
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-03-26 15:08:05 +0100

    updating product route

--- api/routes/product.js
+++ api/routes/product.js
@@ -1,11 +1,14 @@
 //Router
 const router = require('express').Router();
 const {verfiyTokenAndAutho, verfiyTokenAndAdmin} = require('../middleware/verifyToken'); 
+const Product = require('../models/Product');
 const User = require('../models/Product'); 
 
 
 
 
+
+//adding new product to dataBase
 router.post('/' , verfiyTokenAndAdmin , async (req , res)=>{
     try {
         const newProd = new Product(req.body); 
@@ -13,6 +16,26 @@ router.post('/' , verfiyTokenAndAdmin , async (req , res)=>{
 
         res.status(200).json(savedProd); 
 
+    } catch (error) {
+        res.status(500).json({message : error}); 
+    }
+});
+
+
+
+
+
+//updating a product 
+router.put('/:id' , verfiyTokenAndAdmin , async (req , res)=>{
+    try {
+        const {id} = req.params; 
+
+        const updatedProd = await Product.findByIdAndUpdate(id , {
+            $set : req.body , 
+        } , {new : true}); 
+
+
+        res.status(200).json(updatedProd); 
     } catch (error) {
         res.status(500).json({message : error}); 
     }
